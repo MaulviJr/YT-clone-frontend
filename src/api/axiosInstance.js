e9c663e396bcfdba6212e544911d7f9028d6f9f5
MaulviJr <abdulhadighouri123@gmail.com>
2026-06-03 17:36:58 +0500

    mportant fix

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     // Replace with your actual backend URL
-    baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api/v1",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     // This is crucial for your backend to receive/set cookies like AccessToken
     withCredentials: true, 
     
